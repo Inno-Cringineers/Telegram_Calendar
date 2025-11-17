691a893cae0a21150271a0688a08bad375a7f026
TenD <rydenko.urii@mail.ru>
2025-11-17 22:09:39 +0300

    add cicd

---
+++ app/src/tests/__init__.py
@@ -0,0 +1,2 @@
+"""Tests package for Telegram Calendar Bot."""
+
---
+++ app/src/tests/test_hello_world.py
@@ -0,0 +1,7 @@
+"""Basic hello world test to verify CI/CD pipeline."""
+
+
+def test_hello_world():
+    """Simple hello world test to verify pytest works."""
+    assert True, "Hello World test passed!"
+
