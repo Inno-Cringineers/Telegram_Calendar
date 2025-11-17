d0dda2ac33007c41a4f612aad14d53a84995e88c
TenD <rydenko.urii@mail.ru>
2025-11-17 22:19:03 +0300

    add fail test for check cicd

--- app/src/tests/test_hello_world.py
+++ app/src/tests/test_hello_world.py
@@ -4,3 +4,7 @@
 def test_hello_world():
     """Simple hello world test to verify pytest works."""
     assert True, "Hello World test passed!"
+
+def test_hello_world_2():
+    """Simple hello world test to verify pytest works."""
+    assert False, "Hello World test failed!"
